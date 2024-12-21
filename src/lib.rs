0cfeaeb0430e39cef185eadd5e9c477f4b5b7931
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-12-21 17:20:18 -0300

    add base64 decode

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
+use base64::prelude::*;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
-    log(&encoded_file.into());
+    log(&"Grascale called".into());
+
+    let base64_to_vector = BASE64_STANDARD.decode(&encoded_file).unwrap();
+    log(&"Image decoded".into());
 }
