963a5dc2619ac7fa88f691d017a3b93e6ae55b78
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-12-21 17:43:53 -0300

    loading image from memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 use base64::prelude::*;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
@@ -8,4 +9,9 @@ pub fn grayscale(encoded_file: &str) {
 
     let base64_to_vector = BASE64_STANDARD.decode(&encoded_file).unwrap();
     log(&"Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded from memory".into());
+    img = img.grayscale();
+    log(&"Grayscale effect applied".into());
 }
