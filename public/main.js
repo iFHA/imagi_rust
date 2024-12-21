5f764e577031b68c724fcabe1ffe2187dd84c64f
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-12-21 14:47:19 -0300

    add reading file functionality

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
-console.log('test');
+"use-strict";
+
+(function init() {
+    const input = document.getElementById('upload');
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = () => {
+        const base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+        console.log(input.files[0], base64);
+    };
+
+    input.addEventListener('change', () => {
+        fileReader.readAsDataURL(input.files[0]);
+    });
+})();
