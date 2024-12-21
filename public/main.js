d55de73ae7311b9b67c83dca69ff361f5f86287e
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-12-21 16:58:43 -0300

    integrating js with rust

--- public/main.js
+++ public/main.js
@@ -1,12 +1,22 @@
 "use-strict";
 
-(function init() {
+(async function init() {
+
+    let rustApp = null;
+
+    try {
+        rustApp = await import('../pkg/index.js');
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+
     const input = document.getElementById('upload');
     const fileReader = new FileReader();
 
     fileReader.onloadend = () => {
         const base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
-        console.log(input.files[0], base64);
+        rustApp.grayscale(base64);
     };
 
     input.addEventListener('change', () => {
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use wasm_bindgen::prelude::wasm_bindgen;
+use web_sys::console::log_1 as log;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn grayscale(encoded_file: &str) {
+    log(&encoded_file.into());
 }
